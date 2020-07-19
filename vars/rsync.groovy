24270332d19c7d4da9bbbca3fab623fd4a61343b
Markus Schulte <markus>
2020-07-19 18:38:21 +0200

    13

--- vars/rsync.groovy
+++ vars/rsync.groovy
@@ -1,11 +1,4 @@
 def call(def args) {
 
     sh 'printenv'
-
-    // Any valid steps can be called from this code, just like in other
-    // Scripted Pipeline
-    withEnv(["JAVA_HOME=${javaHome}", "PATH+MAVEN=${mvnHome}/bin:${script.env.JAVA_HOME}/bin"]) {
-        sh 'printenv'
-    }
-    sh "rsync --exclude '*/.git' -rt ${source}/* jenkins@localhost::${module}/${documentRoot}"
 }
