d86b6d5f68cf74a8d7b6a5504fc02e757b621730
Markus Schulte <markus>
2020-07-19 18:30:02 +0200

    10

--- vars/rsync.groovy
+++ vars/rsync.groovy
@@ -1,12 +1,10 @@
 def call(def args) {
 
     documentRoot = script.env.DOCUMENT_ROOT_ABSOLUT.replace(modulePath, '')
-    source       = script.env.WORKSPACE.replace(' ', "\\ ")
+    source       = script.env.WORKSPACE.replace(' ', '\\ ')
 
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    rsyncCredentials = credentials('buehne3-jenkins-rsync-password')
-    rsyncCredentials.password
     withEnv(["JAVA_HOME=${javaHome}", "PATH+MAVEN=${mvnHome}/bin:${script.env.JAVA_HOME}/bin"]) {
         sh 'printenv'
     }
