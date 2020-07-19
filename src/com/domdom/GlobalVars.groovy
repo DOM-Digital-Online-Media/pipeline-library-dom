2cfd463b9f2c798f9bf22600cb8b747d0ff8d9e5
Markus Schulte <markus>
2020-07-19 15:52:02 +0200

    introduced rsync

--- src/com/domdom/GlobalVars.groovy
+++ src/com/domdom/GlobalVars.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
-package com.cleverbuilder
+package com.domdom
 
 class GlobalVars {
    static String foo = "bar"
 
    // refer to this in a pipeline using:
    //
-   // import com.cleverbuilder.GlobalVars
+   // import com.domdom.GlobalVars
    // println GlobalVars.foo
 }
---
+++ vars/rsync.groovy
@@ -0,0 +1,15 @@
+String def rsync(
+    String module  = 'webroot',
+    String modulePath = '/var/www/htdocs' ){
+
+    documentRoot = script.env.DOCUMENT_ROOT_ABSOLUT.replace(modulePath'/', '')
+    source       = script.env.WORKSPACE.replace(' ', "\\ ")
+
+    // Any valid steps can be called from this code, just like in other
+    // Scripted Pipeline
+    rsyncCredentials = credentials('buehne3-jenkins-rsync-password')
+    rsyncCredentials.password
+    rsyncCredentials.
+
+    sh "rsync --exclude '*/.git' -rt ${source}/* jenkins@localhost::${module}/${documentRoot}"
+}
